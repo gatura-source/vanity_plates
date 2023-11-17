cdcf6b228c42b2386d156791ba9397ef6c3ded98
Your Name <your.email@example.com>
2023-11-17 17:36:37 +0300

    Fix minor bugs

--- vanity.py
+++ vanity.py
@@ -1,76 +1,59 @@
-from string import ascii_lowercase, ascii_uppercase, digits, punctuation
+"""
+The Script Checks Vanity Plates validity
+"""
+import string
 import sys
 
+DIGITS = string.digits
+ALPHA = string.ascii_uppercase
+SPECIAL_CHARS = string.punctuation
 
-class vanity:
-    def __init__(self, plate):
-        self.plate = plate
-        self.reason = ''
-        self.valid = 0
-        self.got_f_num = 0
-        self.f_num_index = 0
 
-    def check_punc(self):
-        print('Checking for special char')
-        for char in self.plate:
-            if char in punctuation:
-                self.reason = self.reason + 'Contains a special character'
-                self.terminate()
-        self.check_len()
 
-    def terminate(self):
-        print('Terminating')
-        if self.valid:
-            print(f"The Plate {self.plate} is valid")
-            sys.exit(0)
+def check_len(stack):
+    if len(stack) > 6 or len(stack) < 2:
+        return False
+    return True
+def op_check_first_two(stack):
+    top_= stack.pop(0)
+    next_top = stack.pop(0)
+    top_two = top_ + next_top
+    for char in top_two:
+        if char not in ALPHA:
+            return False
+    return True
+def check_plate(stack):
+    f_num = False
+    while stack != []:
+        #print(stack)
+        char = stack.pop(0)
+        if char in DIGITS and not f_num and char != '0':
+            f_num = True
+        elif char in ALPHA and f_num:
+            return False
+        elif char == '0' and not f_num:
+            return False
+        elif char in SPECIAL_CHARS:
+            return False
         else:
-            print(f"The plate {self.plate} is not valid\nReason :: {self.reason}")
+            continue
+    return True
+stack = list(input('Enter your plate here:: '))
+for c in stack:
+    if c in ALPHA:
+        c = c.upper()
+    else:
+        c = c
+ops = {1: check_len,
+       3: op_check_first_two,
+       5: check_plate,
+       2: 'len > 6 or len < 2',
+       6: 'First 2 chars not alphabet',
+       10:'Either contains a special char, or 0 as the first number or char after first number'}
+for k, v in ops.items():
+    if k in range(1, 6, 2):
+        res = v(stack)
+        if res == False:
+            print(f"Terminated.\nReason:: Plate {ops[k*2]}")
             sys.exit(0)
-
-    def check_len(self):
-        print('Checking length')
-        if len(self.plate) > 6 or len(self.plate) < 2:
-            self.reason = self.reason + 'Length > 6 or Length < 2'
-            self.terminate()
-        else:
-            self.check_first_2()
-
-    def check_first_2(self):
-        print('Checking first 2 chars')
-        for char in self.plate[:2]:
-            if char not in ascii_uppercase:
-                self.reason = self.reason + 'First 2 Chars not alphabet'
-                self.terminate()
-            else:
-                for char in self.plate[2:]:
-                    if char in digits and char != '0':
-                        self.got_first_num = 1
-                        self.f_num_index = self.plate.index(char)
-                        #print(self.f_num_index)
-                        self.check_chars_after_f_num()
-                    elif char in ascii_uppercase and self.got_f_num == 1:
-                        self.reason = self.reason + 'Alphabet after first digit character'
-                        self.terminate()
-                    elif char == '0' and self.got_f_num == 0:
-                        self.reason = self.reason + '0 is the first number'
-                        self.terminate()
-                    else:
-                        pass
-
-    def check_chars_after_f_num(self):
-        print('Checking for characters after first number')
-        for char in self.plate[self.f_num_index:]:
-            #print(self.plate[self.f_num_index:])
-            if char in ascii_uppercase:
-                self.terminate()
-        self.valid = 1
-        self.terminate()
-
-plate = input('Enter your vanity plate here:: ')
-check_plate = vanity(str(plate))
-check_plate.check_punc()
-
-
-
-            
-
+print("Plate is valid")
